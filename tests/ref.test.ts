ffc4d6f98eec6c34043761a40fa54e53308c7507
Weslei Ramos <wesleiramos2@gmail.com>
2023-03-13 23:35:42 -0300

    tests: create useRef tests

---
+++ tests/ref.test.ts
@@ -0,0 +1,27 @@
+import { hooked, useState, useRef } from 'lib/hooks'
+import { describe, expect, test } from '@jest/globals'
+
+type Reference = {
+  current: boolean
+}
+
+describe('useRef', () => {
+  test('should keep the same reference', async() => {
+    let reference: Reference | null = null
+
+    const ref = await new Promise(resolve => hooked(() => {
+      const ref = useRef(false)
+      const [ counter, setCounter ] = useState(0)
+
+      if (reference === null) {
+        reference = ref
+      }
+
+      counter <= 5
+        ? setCounter(counter + 1)
+        : resolve(ref)
+    }))
+
+    expect(ref).toBe(reference)
+  })
+})
